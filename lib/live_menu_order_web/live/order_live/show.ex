3a6eba27902848fc9780977e58a18d63c529572b
Kamal Arieff <kamal.arieff@mudah.my>
2022-06-14 21:30:09 +0800

    feat(orders): subscribe to pubsub for show order page

--- lib/live_menu_order_web/live/order_live/show.ex
+++ lib/live_menu_order_web/live/order_live/show.ex
@@ -4,7 +4,8 @@ defmodule LiveMenuOrderWeb.OrderLive.Show do
   alias LiveMenuOrder.Tables
 
   @impl true
-  def mount(_params, _session, socket) do
+  def mount(%{"table_id" => table_id}, _session, socket) do
+    if connected?(socket), do: LiveMenuOrderWeb.Endpoint.subscribe(table_topic(table_id))
     {:ok, socket}
   end
 
@@ -18,6 +19,31 @@ defmodule LiveMenuOrderWeb.OrderLive.Show do
      |> assign(:order, order)}
   end
 
+  @impl true
+  def handle_info(%{event: "save_order"}, socket) do
+    {:noreply,
+     socket
+     |> put_flash(:info, "Order updated.")
+     |> push_patch(to: Routes.order_show_path(socket, :show, socket.assigns.order.table_id))}
+  end
+
+  @impl true
+  def handle_info(%{event: "update_state"}, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "kick"}, socket) do
+    {:noreply,
+     socket
+     |> put_flash(:info, "Order closed.")
+     |> push_patch(to: Routes.menu_index_path(socket, :index, socket.assigns.table.id))}
+  end
+
   defp page_title(:show), do: "Show Order"
   defp page_title(:edit), do: "Edit Order"
+
+  defp table_topic(table_id) do
+    "table:" <> table_id
+  end
 end
