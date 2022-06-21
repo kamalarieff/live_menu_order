a7eda99575a46c8be353df161e389b017f5e334c
Kamal Arieff <kamal.arieff@mudah.my>
2022-06-21 19:51:42 +0800

    wip smoother drag gesture

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,33 +19,132 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
-import { CountUp } from 'countup.js';
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
+import { CountUp } from "countup.js";
 import Alpine from "alpinejs";
-import { DragGesture } from '@use-gesture/vanilla';
+import { DragGesture } from "@use-gesture/vanilla";
 
 window.Alpine = Alpine;
 
-Alpine.data('cart', () => ({
+Alpine.data("cart", () => ({
   isOpen: false,
+  height: 0,
   init() {
-    new DragGesture(this.$el, () => {
-      if (this.isOpen == false ) {
-        // TODO: this looks better when there is a setTimeout
-        this.$el.scrollTop = 0;
-       
-        this.isOpen = true;
-      } else {
-        // TODO: this looks better when there is a setTimeout
-        this.$el.scrollTop = 0;
-       
-        this.isOpen = false;
+    this.height = window.innerHeight - 100;
+
+    gsap.from(this.$el, {
+      y: window.innerHeight,
+      opacity: 0
+    })
+
+    this.$watch("height", (height) => {
+      console.log("height", height);
+    });
+
+    const open = () => {
+      console.log("in here open");
+
+      gsap.to(this.$el, {
+        y: 100,
+        duration: 0.1,
+        onComplete: () => {
+          // this.height = 100;
+          this.isOpen = true;
+        },
+      });
+    };
+
+    const close = () => {
+      console.log("in here close");
+
+      gsap.to(this.$el, {
+        y: window.innerHeight - 100,
+        duration: 0.1,
+        onComplete: () => {
+          // this.height = window.innerHeight - 100;
+          this.isOpen = false;
+        },
+      });
+    };
+
+    new DragGesture(
+      this.$el,
+      ({
+        movement: [, my],
+        velocity: [, vy],
+        direction: [, dy],
+        last,
+        xy: [, y],
+        initial: [, initialY],
+      }) => {
+        const currentHeight = this.height;
+
+        console.log({ y, initialY, my });
+
+        if (this.isOpen == false) {
+          gsap.to(this.$el, {
+            y: y + my,
+            duration: 0.3,
+            onComplete: () => {
+              if (last) {
+                if (dy >= 0) {
+                  close();
+                } else if (vy > 0.3) {
+                  open();
+                } else {
+                  close();
+                }
+              }
+              // else {
+              //   this.height = currentHeight + my;
+              // }
+            },
+          });
+        } else {
+          gsap.to(this.$el, {
+            y: 100 + my,
+            duration: 0.3,
+            onComplete: () => {
+              if (last) {
+                my > window.innerHeight * 0.5 || (vy > 0.3 && dy >= 0)
+                  ? close()
+                  : open();
+              }
+              // else {
+              //   this.height = currentHeight + my;
+              // }
+            },
+          });
+        }
+
+        if (last) {
+          console.log('in here last')
+        }
+        console.log('in here still dragging?')
+        
+
+
+        // if (this.height >= 100 && my < 0) {
+        //   gsap.to(this.$el, {
+        //     y: currentHeight + my,
+        //   });
+        //   this.height = this.height + my;
+        // } else if (this.height <= window.innerHeight - 100 && my > 0) {
+        //   gsap.to(this.$el, {
+        //     y: currentHeight + my,
+        //   });
+        //   this.height = this.height + my;
+        // }
+      },
+      {
+        axis: "y",
+        rubberband: true,
       }
-    }, { axis: 'y', axisThreshold: 2 });
+    );
   },
 }));
 
@@ -56,54 +155,55 @@ Hooks.TotalNumber = {
   mounted() {
     let demo = new CountUp(this.el, this.el.dataset.total, {
       duration: 1,
-      decimalPlaces: 2,
-      prefix: 'RM ',
+      decimalPlaces: 2,
+      prefix: "RM ",
     });
-    demo.start();
+    demo.start();
 
-    this.handleEvent('update_state', e => {
+    this.handleEvent("update_state", (e) => {
       demo.update(e.total);
     });
-  }
-}
+  },
+};
 
 Hooks.LastAdded = {
   mounted() {
     this.handleEvent("update_state", () => {
       gsap.from(this.el, {
         y: 25,
-        ease: 'power1.inOut',
+        ease: "power1.inOut",
         opacity: 0,
-        duration: 0.3
+        duration: 0.3,
       });
     });
   },
 };
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
 let liveSocket = new LiveSocket("/live", Socket, {
-  params: {_csrf_token: csrfToken},
+  params: { _csrf_token: csrfToken },
   hooks: Hooks,
   dom: {
     onBeforeElUpdated(from, to) {
       if (from._x_dataStack) {
-        window.Alpine.clone(from, to)
+        window.Alpine.clone(from, to);
       }
-    }
-  }
+    },
+  },
 });
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (info) => topbar.show());
+window.addEventListener("phx:page-loading-stop", (info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
